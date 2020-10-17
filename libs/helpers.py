4033e0f09c3f53acb90635c05568aee1a1bf557f
Andreas <3765075+Clusters@users.noreply.github.com>
2020-10-17 23:37:47 +0200

    [Bugfix] Config file location hiccups

--- libs/helpers.py
+++ libs/helpers.py
@@ -2,8 +2,11 @@ import os
 import json
 
 def get_config() -> dict:
-    if not os.path.isfile("config/config.json"):
+    path = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(path, "../config/config.json")
+
+    if not os.path.isfile(config_path):
         raise FileNotFoundError("The file config.json could not be found inside the config folder. Have you set up the config already?")
-    with open("config/config.json") as f:
+    with open(config_path) as f:
         data = json.loads(f.read())
     return data
